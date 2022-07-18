9229ffb59deb57fd8033109bc9d36b0c06620c33
Hycient195 <onyeukwuhycient@gmail.com>
2022-07-18 23:42:44 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title centralize">React Hooks</h1>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
